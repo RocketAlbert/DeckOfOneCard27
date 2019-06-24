89ff921b6f35dbcda45bc61323e152a27df3bf6b
rocketalbert <rocketalbert114@gmail.com>
2019-06-24 11:13:56 -0600

    Card Model Completed

---
+++ DeckOfOneCard27/DeckOfOneCard27/Models/Card.swift
@@ -0,0 +1,23 @@
+//
+//  Card.swift
+//  DeckOfOneCard27
+//
+//  Created by Albert Yu on 6/24/19.
+//  Copyright © 2019 AlbertLLC. All rights reserved.
+//
+
+import Foundation
+
+class Card: Decodable {
+    let suit: String
+    let value: String
+    let code: String
+    let image: String
+    
+    init (suit: String, value: String, code: String, image: String) {
+        self.suit = suit
+        self.value = value
+        self.code = code
+        self.image = image
+    }
+}
